6168245d15c7c2d42894cedf3da05763d9ec9880
A Junior <jr@ieee.org>
2019-12-29 12:17:16 -0300

    added makejson assignment

--- makejson.go
+++ makejson.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var name string
+	var address string
+
+	data := make(map[string]string)
+
+	input := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Type a name: ")
+	name, err := input.ReadString('\n')
+	if err != nil {
+		os.Exit(1)
+	}
+	name = strings.TrimSuffix(name, "\n")
+	data["name"] = name
+
+	fmt.Print("Type an address: ")
+	address, err = input.ReadString('\n')
+	if err != nil {
+		os.Exit(1)
+	}
+	address = strings.TrimSuffix(address, "\n")
+	data["address"] = address
+
+	output, err := json.Marshal(data)
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println(string(output))
+}
